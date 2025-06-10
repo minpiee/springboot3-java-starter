94ecacc3dad5a1453a3f688b22ec3f9bb03e12a9
minpiee <mjkang0715@gmail.com>
2025-06-10 16:14:38 +0900

    feat: 블로그 등록 기능 만들기

--- src/main/java/com/boot/spring/controller/PostController.java
+++ src/main/java/com/boot/spring/controller/PostController.java
@@ -1,15 +1,32 @@
 package com.boot.spring.controller;
 
+import com.boot.spring.dto.PostRequestDTO;
+import com.boot.spring.service.PostService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/posts")
 public class PostController {
 
+  private final PostService postService;
+
+  // 생성자가 하나뿐인 경우, @Autowired 생략 가능 (Spring 4.3 이상)
+  public PostController(PostService postService) {
+    this.postService = postService;
+  }
+
   @GetMapping("/new")
   public String showCreateForm() {
     return "post/create";
+  }
+
+  @PostMapping
+  public String createPost(@ModelAttribute PostRequestDTO postRequestDTO) {
+    Long id = postService.createPost(postRequestDTO);
+    return "redirect:/posts/" + id;
   }
 }
